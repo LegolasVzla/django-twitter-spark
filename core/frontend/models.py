cb4b8d02056bbe3204aba754e9ee69597044a80f
Manuel Carrero <mcomercio.web@gmail.com>
2019-06-30 10:13:25 -0400

    Generating general models

--- core/frontend/models.py
+++ core/frontend/models.py
@@ -1,6 +1,8 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from datetime import datetime
 User = get_user_model()
 
 # Create your models here.
@@ -9,35 +11,35 @@ class Languages(models.Model):
 	is_active = models.BooleanField(default=True)
 	is_deleted = models.BooleanField(default=False)
 	updated_date=models.DateTimeField(auto_now=True)
-	created_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=datetime.now)
 
 class Dictionaries(models.Model):
-    NEUTRAL = 'None'
-    POSITIVE = 'P'
-    NEGATIVE = 'N'
-    POLARITY_CHOICES = [
-        (NEUTRAL, 'Neutral'),
-        (POSITIVE, 'Positive'),
-        (NEGATIVE, 'Negative'),
-    ]
+	NEUTRAL = 'None'
+	POSITIVE = 'P'
+	NEGATIVE = 'N'
+	POLARITY_CHOICES = [
+	    (NEUTRAL, 'Neutral'),
+	    (POSITIVE, 'Positive'),
+	    (NEGATIVE, 'Negative'),
+	]
 	#__polarity_values=((1,_('Positive')),(2,_('Negative')),(3,_('Neutral')))
 	word = models.CharField( max_length = 100)
-    polarity = models.CharField(
-        max_length=4,
-        choices=POLARITY_CHOICES,
-        default=NEUTRAL,
-    )
+	polarity = models.CharField(
+	    max_length=4,
+	    choices=POLARITY_CHOICES,
+	    default=NEUTRAL,
+	)
 	#polarity = models.CharField(choices = __polarity_values)
 	language_id = models.ForeignKey(Languages,related_name='dictionaries_language_id',on_delete=models.CASCADE)
 	is_active = models.BooleanField(default=True)
 	is_deleted = models.BooleanField(default=False)
 	updated_date=models.DateTimeField(auto_now=True)
-	created_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=datetime.now)
 
 	def __str__(self):
 		return self.word
- 	def __unicode__(self ):
- 		return self.word
+	def __unicode__(self ):
+		return self.word
 
 class CustomDictionaries(models.Model):
     NEUTRAL = 'None'
@@ -48,47 +50,47 @@ class CustomDictionaries(models.Model):
         (POSITIVE, 'Positive'),
         (NEGATIVE, 'Negative'),
     ]
-	user_id = models.ForeignKey(User,related_name='custom_dictionaries_user_id',on_delete=models.CASCADE)
-	word = models.CharField( max_length = 100)
+    user_id = models.ForeignKey(User,related_name='custom_dictionaries_user_id',on_delete=models.CASCADE)
+    word = models.CharField( max_length = 100)
     polarity = models.CharField(
-        max_length=4,
-        choices=POLARITY_CHOICES,
-        default=NEUTRAL,
-    )	
-	language_id = models.ForeignKey(Languages,related_name='custom_dictionaries_language_id',on_delete=models.CASCADE)
-	is_active = models.BooleanField(default=True)
-	is_deleted = models.BooleanField(default=False)
-	updated_date=models.DateTimeField(auto_now=True)
-	created_date = models.DateTimeField(default=timezone.now)
+		max_length=4,
+		choices=POLARITY_CHOICES,
+		default=NEUTRAL,
+	)
+    language_id = models.ForeignKey(Languages,related_name='custom_dictionaries_language_id',on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    updated_date=models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(default=datetime.now)
 
-	def __str__(self):
-		return self.word
- 	def __unicode__(self ):
- 		return self.word
+    def __str__(self):
+    	return self.word
+    def __unicode__(self ):
+    	return self.word
 
 class SocialNetwork(models.Model):
 	name = models.CharField( max_length = 100)
 	is_active = models.BooleanField(default=True)
 	is_deleted = models.BooleanField(default=False)
 	updated_date=models.DateTimeField(auto_now=True)
-	created_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=datetime.now)
 
 class Searched(models.Model):
-    NEUTRAL = 'None'
-    POSITIVE = 'P'
-    NEGATIVE = 'N'
-    POLARITY_CHOICES = [
-        (NEUTRAL, 'Neutral'),
-        (POSITIVE, 'Positive'),
-        (NEGATIVE, 'Negative'),
-    ]
+	NEUTRAL = 'None'
+	POSITIVE = 'P'
+	NEGATIVE = 'N'
+	POLARITY_CHOICES = [
+		(NEUTRAL, 'Neutral'),
+		(POSITIVE, 'Positive'),
+		(NEGATIVE, 'Negative'),
+	]
 	user_id = models.ForeignKey(User,related_name='searched_user_id',on_delete=models.CASCADE)
 	word = models.CharField( max_length = 100)
-    polarity = models.CharField(
-        max_length=4,
-        choices=POLARITY_CHOICES,
-        default=NEUTRAL,
-    )	
+	polarity = models.CharField(
+	    max_length=4,
+	    choices=POLARITY_CHOICES,
+	    default=NEUTRAL,
+	)	
 	topic = models.CharField( max_length = 100, null = True)
 	liked = models.IntegerField(default=0)	
 	retweeted = models.IntegerField(default=0)	
@@ -96,12 +98,12 @@ class Searched(models.Model):
 	is_active = models.BooleanField(default=True)
 	is_deleted = models.BooleanField(default=False)
 	updated_date=models.DateTimeField(auto_now=True)
-	created_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=datetime.now)
 
 	def __str__(self):
 		return self.word
- 	def __unicode__(self ):
- 		return self.word
+	def __unicode__(self ):
+		return self.word
 
 class Topics(models.Model):
 	name = models.CharField( max_length = 100)
@@ -109,12 +111,12 @@ class Topics(models.Model):
 	is_active = models.BooleanField(default=True)
 	is_deleted = models.BooleanField(default=False)
 	updated_date=models.DateTimeField(auto_now=True)
-	created_date = models.DateTimeField(default=timezone.now)
-
+	created_date = models.DateTimeField(default=datetime.now)
+	
 class WordRoots(models.Model):
 	word_root = models.CharField( max_length = 100)
 	topics_id = models.ForeignKey(Topics,related_name='word_roots_topics_id',on_delete=models.CASCADE)
 	is_active = models.BooleanField(default=True)
 	is_deleted = models.BooleanField(default=False)
 	updated_date=models.DateTimeField(auto_now=True)
-	created_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=datetime.now)
