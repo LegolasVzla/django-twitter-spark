6504733765497709ba86e59b751714619b6093a8
Manuel Carrero <mcomercio.web@gmail.com>
2019-11-30 23:55:52 -0400

    Adding DynamicFieldsModelSerializer to test required and optionals fields

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,6 +4,41 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+class DynamicSerializerMixin:
+    """
+    A Serializer that takes an additional `fields` argument that
+    controls which fields should be used.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop("fields", None)
+        excluded_fields = kwargs.pop("excluded_fields", None)
+        required_fields = kwargs.pop("required_fields", None)
+
+        # Instantiate the superclass normally
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+            if isinstance(fields, dict):
+                for field, config in fields.items():
+                    set_attrs(self.fields[field], config)
+
+        if excluded_fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            for field_name in excluded_fields:
+                self.fields.pop(field_name)
+
+        if required_fields is not None:
+            for field_name in required_fields:
+                self.fields[field_name].required = True
+
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
@@ -15,7 +50,7 @@ class UserDetailsSerializer(serializers.ModelSerializer):
 		model = User
 		fields = ('user',)
 
-class UserProfileUpdateSerializer(serializers.ModelSerializer):
+class UserProfileUpdateSerializer(DynamicSerializerMixin,serializers.ModelSerializer):
 	first_name = serializers.CharField(required=False, allow_blank=True, max_length=100)
 	last_name = serializers.CharField(required=False, allow_blank=True, max_length=100)	
 	class Meta:
