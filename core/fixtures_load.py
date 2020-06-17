0d8985704fb79581b4b9b440a0e830f5850ac427
Manuel Carrero <mcomercio.web@gmail.com>
2020-06-16 21:22:52 -0400

    Updating fixtures_load.py file

--- core/fixtures_load.py
+++ core/fixtures_load.py
@@ -8,8 +8,8 @@ def loaddata():
 	subprocess.call("python manage.py loaddata fixtures/"+'users.json',shell=True)
 	subprocess.call("python manage.py loaddata fixtures/"+'topics.json',shell=True)
 	subprocess.call("python manage.py loaddata fixtures/"+'word_root.json',shell=True)
-	subprocess.call("python manage.py loaddata fixtures/"+'positive_spanish_dictionary.json',shell=True)
-	subprocess.call("python manage.py loaddata fixtures/"+'negative_spanish_dictionary.json',shell=True)
+	subprocess.call("python manage.py loaddata fixtures/"+'positive_dictionary.json',shell=True)
+	subprocess.call("python manage.py loaddata fixtures/"+'negative_dictionary.json',shell=True)
 	subprocess.call("python manage.py loaddata fixtures/"+'social_network_accounts.json',shell=True)
 	
 loaddata()
